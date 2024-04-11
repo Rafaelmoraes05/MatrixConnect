224c02f5687acd09de12a00d8be42211537e996e
Rafael Moraes <rafaelmoraes005@gmail.com>
2024-04-11 19:38:19 -0300

    Refatoração: Adiciona método para obter caminho de arquivo via entrada do usuário
    - Adiciona método 'obterCaminhoArquivo()' na classe LeitorArquivo para solicitar ao usuário o caminho do arquivo de forma interativa.
    
    - Ajusta o método 'lerArquivo()' na classe HistoricoAcademico para utilizar o caminho do arquivo fornecido.

--- src/SistemaNovaMatriz/LeitorArquivo.java
+++ src/SistemaNovaMatriz/LeitorArquivo.java
@@ -1,28 +1,11 @@
 package SistemaNovaMatriz;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class LeitorArquivo {
-    public static List<String> lerHistoricoAcademico(String caminhoArquivo) {
-        List<String> disciplinasAprovadas = new ArrayList<>();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(caminhoArquivo))) {
-            String linha;
-
-            while ((linha = reader.readLine()) != null) {
-                // Verificar se a linha contém informações de disciplina aprovada
-                if (linha.contains("Aprovado")) {
-                    disciplinasAprovadas.add(linha);
-                }
-            }
-        } catch (IOException e) {
-            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
-        }
-
-        return disciplinasAprovadas;
+    public static String obterCaminhoArquivo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o caminho do arquivo:");
+        return scanner.nextLine();
     }
 }
