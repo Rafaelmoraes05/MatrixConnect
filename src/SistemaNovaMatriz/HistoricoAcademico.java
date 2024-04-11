e2ef240562e9de62f80efe0b61923775689cf35d
Rafael Moraes <rafaelmoraes005@gmail.com>
2024-04-11 19:39:01 -0300

    Refatoração: Ajusta método lerArquivo para tratar disciplinas cursadas na matriz curricular
    - Adiciona verificação para identificar o início das disciplinas cursadas na matriz curricular.
    - Ajusta o loop de leitura do arquivo para processar apenas as linhas relevantes.
    - Implementa lógica para extrair e formatar corretamente os códigos e nomes das disciplinas.
    - Adiciona tratamento para interromper a leitura após encontrar a palavra-chave "OBR".

--- src/SistemaNovaMatriz/HistoricoAcademico.java
+++ src/SistemaNovaMatriz/HistoricoAcademico.java
@@ -1,23 +1,51 @@
 package SistemaNovaMatriz;
 
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class HistoricoAcademico {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o caminho do arquivo de histórico acadêmico:");
-        String caminhoArquivo = scanner.nextLine();
-
-        List<String> disciplinasAprovadas = LeitorArquivo.lerHistoricoAcademico(caminhoArquivo);
-
-        // Exibir disciplinas aprovadas
-        System.out.println("Disciplinas Aprovadas:");
-        for (String disciplina : disciplinasAprovadas) {
-            System.out.println(disciplina);
-        }
-
-        scanner.close();
+        String caminho = LeitorArquivo.obterCaminhoArquivo();
+        lerArquivo(caminho);
     }
 
+    private static void lerArquivo(String caminho) {
+        try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+            String linha;
+
+            boolean iniciouDisciplinas = false;
+
+            while ((linha = br.readLine()) != null) {
+
+                if (linha.contains("Disciplinas cursadas na matriz curricular:")) {
+
+                    iniciouDisciplinas = true;
+                    continue;
+                }
+                if (iniciouDisciplinas && !linha.trim().isEmpty()) {
+                    if (linha.contains("Aprovado")) {
+                        String[] partes = linha.split("\\s+");
+                        String codigo = partes[2];
+                        String discplina = "";
+                        for (int i=3; i<partes.length; i++){
+
+                            if (!partes[i].equals("OBR")) {
+                                discplina=discplina.concat(" ");
+                                discplina=discplina.concat(partes[i]);
+
+                            }else {
+                                break;
+                            }
+                        }
+                        System.out.println(codigo + "\t" + discplina);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
