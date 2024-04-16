ce63f6c621721d2c738ce12eb59c85122360075b
Rafael Moraes <rafaelmoraes005@gmail.com>
2024-04-16 14:20:05 -0300

    Atualizando a classe HistoricoAcademico para ele retornar uma lista de Disciplinas do Historico do Aluno.

--- src/SistemaNovaMatriz/HistoricoAcademico.java
+++ src/SistemaNovaMatriz/HistoricoAcademico.java
@@ -3,16 +3,14 @@ package SistemaNovaMatriz;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistoricoAcademico {
 
-    public static void main(String[] args) {
+    private static List<Disciplina> retornarListaDeDisciplinasDoHistorico() {
+        List<Disciplina> disciplinasHistorico = new ArrayList<>();
         String caminho = LeitorArquivo.obterCaminhoArquivo();
-        lerArquivo(caminho);
-    }
-
-    private static void lerArquivo(String caminho) {
         try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
             String linha;
 
@@ -30,22 +28,24 @@ public class HistoricoAcademico {
                         String[] partes = linha.split("\\s+");
                         String codigo = partes[2];
                         String discplina = "";
-                        for (int i=3; i<partes.length; i++){
+                        for (int i = 3; i < partes.length; i++) {
 
                             if (!partes[i].equals("OBR")) {
-                                discplina=discplina.concat(" ");
-                                discplina=discplina.concat(partes[i]);
+                                discplina = discplina.concat(" ");
+                                discplina = discplina.concat(partes[i]);
 
-                            }else {
+                            } else {
                                 break;
                             }
                         }
                         System.out.println(codigo + "\t" + discplina);
+                        disciplinasHistorico.add(new Disciplina(codigo, discplina, 0));
                     }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return disciplinasHistorico;
     }
 }
